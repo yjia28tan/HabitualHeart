6c1a9eabee06454cef90d64c9f04f3e5ec494e68
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-04 18:34:04 +0800

    Update floating button navigate to new habit page

--- habitual_heart_app/lib/pages/habits_page.dart
+++ habitual_heart_app/lib/pages/habits_page.dart
@@ -5,6 +5,7 @@ import 'package:habitual_heart_app/design/font_style.dart';
 import '/design/font_style.dart';
 import '/widgets/textfield_style.dart';
 import '/pages/signin_page.dart';
+import '/pages/new_habit_page.dart';
 
 class HabitsPage extends StatefulWidget {
   static String routeName = '/HabitsPage';
@@ -19,21 +20,27 @@ class _HabitsPageState extends State<HabitsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false, //disable back button
-        title: Text(
-          "Habits",
-          style: headerText,
+        appBar: AppBar(
+          automaticallyImplyLeading: false, //disable back button
+          title: Text(
+            "Habits",
+            style: headerText,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xFF366021),
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        onPressed: () {},
-      )
-    );
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xFF366021),
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (child) => NewHabitPage(),
+              ),
+            );
+          },
+        ));
   }
 }
---
+++ habitual_heart_app/lib/pages/new_habit_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewHabitPage extends StatefulWidget{
+
+  const NewHabitPage({Key?key}):super(key:key);
+
+  @override
+  State<NewHabitPage> createState() => _NewHabitPageState();
+}
+
+class _NewHabitPageState extends State<NewHabitPage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
