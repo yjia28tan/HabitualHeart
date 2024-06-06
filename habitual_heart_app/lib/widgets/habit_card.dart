51f0249f6eb92cd1e02c660920f603a5994c9969
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-07 00:24:55 +0800

    Create habit_card.dart

---
+++ habitual_heart_app/lib/widgets/habit_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:habitual_heart_app/models/habit_model.dart';
+import 'package:habitual_heart_app/models/habit_record_model.dart';
+
+class HabitCard extends StatelessWidget {
+  const HabitCard({Key? key, required this.habit, required this.record})
+      : super(key: key);
+
+  final HabitModel habit;
+  final HabitRecordModel record;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+          leading: habitCategoryIcon(habit.habitCategory),
+          title: Text(habit.habitName),
+          subtitle: Text(habit.habitCategory),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.local_fire_department,
+                color: record.streak > 0 ? Colors.orange : null,
+              ),
+              Text(record.streak.toString())
+            ],
+          )
+      ),
+    );
+  }
+
+  habitCategoryIcon(String category) {
+    switch (category) {
+      case 'Productivity':
+        return const Icon(Icons.auto_graph);
+      case 'Financial':
+        return const Icon(Icons.attach_money);
+      case 'Growth':
+        return const Icon(Icons.emoji_events_outlined);
+      case 'Health':
+        return const Icon(Icons.monitor_heart_outlined);
+    }
+  }
+}
