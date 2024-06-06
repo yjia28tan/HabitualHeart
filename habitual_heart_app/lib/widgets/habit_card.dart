0a124bdddd36a7c33591c7d4ee9a1c0b59921235
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-07 07:30:51 +0800

    Update habit_card.dart

--- habitual_heart_app/lib/widgets/habit_card.dart
+++ habitual_heart_app/lib/widgets/habit_card.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:habitual_heart_app/models/habit_model.dart';
 import 'package:habitual_heart_app/models/habit_record_model.dart';
+import 'package:habitual_heart_app/pages/edit_habit_page.dart';
 
 class HabitCard extends StatelessWidget {
-  const HabitCard({Key? key, required this.habit, this.record})
-      : super(key: key);
+  const HabitCard({super.key, required this.habit, this.record});
 
   final HabitModel habit;
   final HabitRecordModel? record;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
+    return Dismissible(
+      key: Key(habit.habitID),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              color: Colors.limeAccent,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EditHabitPage(habit: habit)),
+                  );
+                },
+              ),
+            ),
+            Container(
+              color: Colors.lime,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Confirm Delete'),
+                      content: const Text(
+                          'Are you sure you want to delete this habbit?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            deleteHabit(habit);
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Delete'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      confirmDismiss: (direction) async {
+        return false;
+      },
+      onDismissed: (direction) {},
+      child: Card(
+        child: ListTile(
           leading: habitCategoryIcon(habit.habitCategory),
           title: Text(habit.habitName),
           subtitle: Text(habit.habitCategory),
@@ -29,7 +95,9 @@ class HabitCard extends StatelessWidget {
               ),
               record != null ? Text(record!.streak.toString()) : const Text('0')
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 
@@ -45,4 +113,6 @@ class HabitCard extends StatelessWidget {
         return const Icon(Icons.monitor_heart_outlined);
     }
   }
+
+  deleteHabit(HabitModel habit) {}
 }
