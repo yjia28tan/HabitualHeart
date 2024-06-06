6cf5bd831d6aa9e49c591bb75704cc78f5e8bb45
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-07 05:54:18 +0800

    Update habits_page.dart

--- habitual_heart_app/lib/pages/habits_page.dart
+++ habitual_heart_app/lib/pages/habits_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:habitual_heart_app/design/font_style.dart';
+import 'package:habitual_heart_app/main.dart';
+import 'package:habitual_heart_app/models/habit_model.dart';
+import 'package:habitual_heart_app/models/habit_record_model.dart';
+import 'package:habitual_heart_app/widgets/habit_card.dart';
 import '/design/font_style.dart';
 import '/widgets/textfield_style.dart';
 import '/pages/signin_page.dart';
@@ -17,6 +21,71 @@ class HabitsPage extends StatefulWidget {
 }
 
 class _HabitsPageState extends State<HabitsPage> {
+  List<HabitModel> habits = [];
+  late Map<String, HabitRecordModel?> latestRecordsMap;
+  // late HabitRecordModel latestRecords;
+
+  @override
+  void initState() {
+    super.initState();
+    latestRecordsMap = {};
+    fetchHabits();
+  }
+
+  Future<void> fetchHabits() async {
+    try {
+      final uid = globalUID;
+      QuerySnapshot habitSnapshot = await FirebaseFirestore.instance
+          .collection('habits')
+          .where('userID', isEqualTo: uid)
+          .get();
+
+      for (QueryDocumentSnapshot doc in habitSnapshot.docs) {
+        String habitID = doc.id;
+        print("HabitIdddddddddddddddddddddddddd");
+        print(habitID);
+        print(doc.data());
+        HabitModel habit =
+            HabitModel.fromMap(habitID, doc.data() as Map<String, dynamic>);
+        habits.add(habit);
+
+        // Fetch latest record for the habit (if exists) and store it in the map
+        QuerySnapshot recordSnapshot = await FirebaseFirestore.instance
+            .collection('habitRecord')
+            .where('habitID', isEqualTo: habitID)
+            .orderBy('date', descending: true)
+            .limit(1)
+            .get();
+
+        //   if (recordSnapshot.docs.isNotEmpty) {
+        //     for (QueryDocumentSnapshot recordDoc in recordSnapshot.docs) {
+        //       String habitRecordID = recordDoc.id;
+        //       latestRecordsMap[habitID] = HabitRecordModel.fromMap(
+        //           habitRecordID, recordDoc.data() as Map<String, dynamic>);
+        //       print("Habit Record Document Data: ${recordDoc.data()}");
+        //     }
+        //   } else {
+        //     latestRecordsMap[habitID] = null;
+        //   }
+        // }
+        // latestRecords = latestRecordsMap;
+
+        if (recordSnapshot.docs.isNotEmpty) {
+          String habitRecordID = recordSnapshot.docs.first.id;
+          latestRecordsMap[habitID] = HabitRecordModel.fromMap(habitRecordID,
+              recordSnapshot.docs.first.data() as Map<String, dynamic>);
+        } else {
+          latestRecordsMap[habitID] = null;
+        }
+      }
+
+      setState(() {});
+    } catch (e) {
+      print("Error fetching: $e");
+      // Handle error: Display an error message or take appropriate action
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +96,19 @@ class _HabitsPageState extends State<HabitsPage> {
             style: headerText,
           ),
         ),
+        body: habits.isNotEmpty
+            ? ListView.builder(
+                itemCount: habits.length,
+                itemBuilder: (context, index) {
+                  return HabitCard(
+                    habit: habits[index],
+                    record: latestRecordsMap[habits[index].habitID],
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Color(0xFF366021),
           child: Icon(
