ffb482e266c1d9dd206303d907676f3b817aa6ed
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-08 05:20:44 +0800

    Update new_habit_page.dart

--- habitual_heart_app/lib/pages/new_habit_page.dart
+++ habitual_heart_app/lib/pages/new_habit_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habitual_heart_app/design/font_style.dart';
+import 'package:habitual_heart_app/data/habit_category_list.dart';
 
 class NewHabitPage extends StatefulWidget {
   const NewHabitPage({super.key});
@@ -9,14 +10,176 @@ class NewHabitPage extends StatefulWidget {
 }
 
 class _NewHabitPageState extends State<NewHabitPage> {
+  IconData _selectedIcon = Icons.crop_original;
+  String? _selectedCategory;
+  int _habitCount = 1;
+
+  IconData _getIconForCategory(String category) {
+    switch (category) {
+      case 'Productivity':
+        return Icons.auto_graph;
+      case 'Financial':
+        return Icons.attach_money;
+      case 'Growth':
+        return Icons.emoji_events_outlined;
+      case 'Health':
+        return Icons.monitor_heart_outlined;
+      default:
+        return Icons.crop_original;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final categoryItems = habitCategoryListItem
+        .where((category) => category != 'All')
+        .map((category) => DropdownMenuItem(
+              value: category,
+              child: Text(category),
+            ))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, //disable back button
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
         title: Text(
           "New Habit",
           style: headerText,
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(
+              _selectedIcon, // Your big icon
+              size: 100.0,
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Habit Name',
+                style: homeSubHeaderText,
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  hintText: 'Write habit name...',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF366021)))),
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Habit Description',
+                style: homeSubHeaderText,
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                  hintText: 'Write description...',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF366021)))),
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Habit Category',
+                style: homeSubHeaderText,
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            DropdownButtonFormField(
+              value: _selectedCategory,
+              items: categoryItems, // Your category items
+              onChanged: (value) {
+                setState(() {
+                  _selectedCategory = value;
+                  _selectedIcon = _getIconForCategory(value as String);
+                });
+              }, // Implement onChanged
+              decoration: const InputDecoration(
+                  hintText: 'Select category...',
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF366021)))),
+            ),
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Habit Daily Count',
+                style: homeSubHeaderText,
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_habitCount > 1) {
+                        _habitCount--;
+                      }
+                    });
+                  },
+                  icon: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20.0),
+                Text(
+                  '$_habitCount',
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+                const SizedBox(width: 20.0),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _habitCount++;
+                    });
+                  },
+                  icon: const Icon(Icons.add),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Container(
+              height: 45,
+              width: 250,
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: ElevatedButton.icon(
+                icon: Icon(
+                  Icons.check_circle_rounded,
+                  color: theme.bottomNavigationBarTheme.selectedItemColor,
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      theme.bottomNavigationBarTheme.backgroundColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                label: Text(
+                  'Submit',
+                  style: homeSubHeaderText,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ]),
         ),
       ),
     );
