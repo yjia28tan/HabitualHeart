32fc60aa3ba3176883890d58e951adb645fde90b
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-08 03:00:29 +0800

    Update new_habit_page.dart

--- habitual_heart_app/lib/pages/new_habit_page.dart
+++ habitual_heart_app/lib/pages/new_habit_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:habitual_heart_app/design/font_style.dart';
 
-class NewHabitPage extends StatefulWidget{
-
+class NewHabitPage extends StatefulWidget {
   const NewHabitPage({super.key});
 
   @override
@@ -10,7 +10,15 @@ class NewHabitPage extends StatefulWidget{
 
 class _NewHabitPageState extends State<NewHabitPage> {
   @override
-  Widget build(BuildContext context){
-    return const Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, //disable back button
+        title: Text(
+          "New Habit",
+          style: headerText,
+        ),
+      ),
+    );
   }
 }
