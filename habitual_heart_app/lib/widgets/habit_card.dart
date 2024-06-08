988e05e9deeefaf978dd36ed1ac14c7407ddf266
GERMIN <104176182+germin701@users.noreply.github.com>
2024-06-08 20:21:49 +0800

    Update habit_card.dart

--- habitual_heart_app/lib/widgets/habit_card.dart
+++ habitual_heart_app/lib/widgets/habit_card.dart
@@ -1,109 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:habitual_heart_app/models/habit_model.dart';
 import 'package:habitual_heart_app/models/habit_record_model.dart';
 import 'package:habitual_heart_app/pages/edit_habit_page.dart';
 import 'package:habitual_heart_app/pages/habit_check_in_page.dart';
+import 'package:habitual_heart_app/pages/habits_page.dart';
 
-class HabitCard extends StatelessWidget {
+class HabitCard extends StatefulWidget {
   const HabitCard({super.key, required this.habit, this.record});
 
   final HabitModel habit;
   final HabitRecordModel? record;
 
   @override
+  State<HabitCard> createState() => _HabitCardState();
+}
+
+class _HabitCardState extends State<HabitCard> {
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => HabitCheckInPage(habit: habit),
-          ),
-        );
-      },
-      child: Slidable(
-        key: Key(habit.habitID),
-        actionPane: const SlidableScrollActionPane(),
-        secondaryActions: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: SizedBox(
-              height: 63,
-              child: IconSlideAction(
-                caption: 'Edit',
-                color: Colors.lightGreenAccent,
-                icon: Icons.edit,
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => EditHabitPage(habit: habit)),
-                  );
-                },
-              ),
+    return ScaffoldMessenger(
+      key: scaffoldMessengerKey,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HabitCheckInPage(habit: widget.habit),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 3),
-            child: ClipRRect(
+          );
+        },
+        child: Slidable(
+          key: Key(widget.habit.habitID),
+          actionPane: const SlidableScrollActionPane(),
+          secondaryActions: <Widget>[
+            ClipRRect(
               borderRadius: BorderRadius.circular(8),
               child: SizedBox(
                 height: 63,
                 child: IconSlideAction(
-                  caption: 'Delete',
-                  color: Colors.lightGreen,
-                  icon: Icons.delete,
+                  caption: 'Edit',
+                  color: Colors.lightGreenAccent,
+                  icon: Icons.edit,
                   onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('Confirm Delete'),
-                        content: const Text(
-                            'Are you sure you want to delete this habit?'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Cancel'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              deleteHabit(habit);
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Delete'),
-                          ),
-                        ],
-                      ),
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              EditHabitPage(habit: widget.habit)),
                     );
                   },
                 ),
               ),
             ),
-          ),
-        ],
-        child: Card(
-          child: ListTile(
-            leading: habitCategoryIcon(habit.habitCategory),
-            title: Text(habit.habitName),
-            subtitle: Text(habit.habitCategory),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  Icons.local_fire_department,
-                  color: record != null
-                      ? record!.streak > 0
-                          ? Colors.orange
-                          : Colors.grey
-                      : Colors.grey,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 3),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  height: 63,
+                  child: IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.lightGreen,
+                    icon: Icons.delete,
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Confirm Delete'),
+                          content: const Text(
+                              'Are you sure you want to delete this habit?\n\n'
+                              'Note: This will also delete all associated habit records.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                deleteHabit(widget.habit);
+                              },
+                              child: const Text('Delete'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-                record != null
-                    ? Text(record!.streak.toString())
-                    : const Text('0')
-              ],
+              ),
+            ),
+          ],
+          child: Card(
+            child: ListTile(
+              leading: habitCategoryIcon(widget.habit.habitCategory),
+              title: Text(widget.habit.habitName),
+              subtitle: Text(widget.habit.habitCategory),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: widget.record != null
+                        ? widget.record!.streak > 0
+                            ? Colors.orange
+                            : Colors.grey
+                        : Colors.grey,
+                  ),
+                  widget.record != null
+                      ? Text(widget.record!.streak.toString())
+                      : const Text('0')
+                ],
+              ),
             ),
           ),
         ),
@@ -124,5 +138,32 @@ class HabitCard extends StatelessWidget {
     }
   }
 
-  deleteHabit(HabitModel habit) {}
+  void deleteHabit(HabitModel habit) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    try {
+      await firestore.collection('habits').doc(habit.habitID).delete();
+      QuerySnapshot habitRecordsSnapshot = await firestore
+          .collection('habitRecord')
+          .where('habitID', isEqualTo: habit.habitID)
+          .get();
+      for (DocumentSnapshot doc in habitRecordsSnapshot.docs) {
+        await doc.reference.delete();
+      }
+      if (scaffoldMessengerKey.currentContext != null) {
+        Navigator.pushReplacement(
+          scaffoldMessengerKey.currentContext!,
+          MaterialPageRoute(builder: (context) => const HabitsPage()),
+        );
+        ScaffoldMessenger.of(scaffoldMessengerKey.currentContext!).showSnackBar(
+          const SnackBar(content: Text('Habit deleted successfully.')),
+        );
+      }
+    } catch (error) {
+      if (scaffoldMessengerKey.currentContext != null) {
+        ScaffoldMessenger.of(scaffoldMessengerKey.currentContext!).showSnackBar(
+          SnackBar(content: Text('Failed to delete habit: $error')),
+        );
+      }
+    }
+  }
 }
